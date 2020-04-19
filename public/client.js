2064a345d8a31f0aabbd6487eb9e09759c9d18e3
Hugo Avila <hugoaf@yahoo.com.mx>
2020-04-18 19:26:37 -0700

    Update client.js

--- public/client.js
+++ public/client.js
@@ -1,15 +1,31 @@
-$( document ).ready(function() {
-  
-  
-   
+$(document).ready(function () {
+  /*global io*/
+  var socket = io();
+  socket.on('user count', function(data){
+    console.log(data);
+  });
+
+  socket.on('user', function(data){
+    $('#num-users').text(data.currentUsers + ' users online');
+    var message = data.name;
+    if(data.connected){
+      message += ' has joined the chat.';
+    } else {
+      message += ' has left the chat.';
+    }
+    $('#messages').append($('<li>').html('<b>' + message + '<\/b>'));
+  })
+
   // Form submittion with new message in field with id 'm'
-  $('form').submit(function(){
+  $('form').submit(function () {
     var messageToSend = $('#m').val();
+    socket.emit('chat message', messageToSend);
     //send message to server here?
     $('#m').val('');
     return false; // prevent form submit from refreshing page
   });
-  
-  
-  
+
+  socket.on('chat message', (data)=>{
+    $('#messages').append('<li>' + data.user + ": " +  data.message + '</li>');
+  })
 });
